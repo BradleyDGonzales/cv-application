3f235057b74260cb5529450d03f43cc291651a7d
Bradley <gonzales.bradleyd@gmail.com>
2022-12-28 17:41:38 -0500

    Fix formatting

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,10 +9,10 @@ class Education extends Component {
                 {this.props.info.education.map((education, index) => {
                     return (
                         <form className="educationForm">
-                            <input defaultValue={education.schoolName} onChange={(e) => this.props.onCompChange(e,index)} placeholder="Name of school" key={uniqid()} type='text' id={'schoolName'+ index}></input>
-                            <input defaultValue={education.majorName} onChange={(e) => this.props.onCompChange(e,index)} placeholder="Major" key={uniqid()}  type='text' id={'majorName' + index}></input>
-                            <input defaultValue={education.dateEnd} onChange={(e) => this.props.onCompChange(e,index)} placeholder="Year" key={uniqid()}  type='text' id={'dateEnd' + index}></input>
-                            <input defaultValue={education.gpa} onChange={(e) => this.props.onCompChange(e,index)} placeholder="GPA out of 4.0" key={uniqid()} type='number' id={'gpa' + index} min="1.0" max="4.0" step="0.1"></input>
+                            <input defaultValue={education.schoolName} onChange={(e) => this.props.onCompChange(e, index)} placeholder="Name of school" key={uniqid()} type='text' id={'schoolName' + index}></input>
+                            <input defaultValue={education.majorName} onChange={(e) => this.props.onCompChange(e, index)} placeholder="Major" key={uniqid()} type='text' id={'majorName' + index}></input>
+                            <input defaultValue={education.dateEnd} onChange={(e) => this.props.onCompChange(e, index)} placeholder="Year" key={uniqid()} type='text' id={'dateEnd' + index}></input>
+                            <input defaultValue={education.gpa} onChange={(e) => this.props.onCompChange(e, index)} placeholder="GPA out of 4.0" key={uniqid()} type='number' id={'gpa' + index} min="1.0" max="4.0" step="0.1"></input>
                         </form>
                     )
                 })}
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -9,9 +9,9 @@ class Personal extends Component {
                 <form className="infoForm" onSubmit={this.handleSubmit}>
                     <input onChange={this.props.onCompChange} type='text' id='firstName' placeholder="First Name" />
                     <input onChange={this.props.onCompChange} type='text' id='lastName' placeholder="Last Name" />
-                    <input onChange={this.props.onCompChange} type='text' id='title' placeholder="Title"/> 
-                    <input onChange={this.props.onCompChange} type='text' id='email' placeholder="Email"/>
-                    <input onChange={this.props.onCompChange} type='tel' id='phoneNumber' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number (xxx-xxx-xxxx)"/>
+                    <input onChange={this.props.onCompChange} type='text' id='title' placeholder="Title" />
+                    <input onChange={this.props.onCompChange} type='text' id='email' placeholder="Email" />
+                    <input onChange={this.props.onCompChange} type='tel' id='phoneNumber' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number (xxx-xxx-xxxx)" />
                 </form>
             </div>
         )
